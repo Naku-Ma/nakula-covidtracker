a3e95e90a2d7ffb4665f0dc5418d4cae655cab38
Nakula <66574161+Naku-Ma@users.noreply.github.com>
2020-07-14 23:06:03 +0700

    Adding all existing files via upload

---
+++ readme.md
@@ -0,0 +1,12 @@
+COVID PART -3 COACHING CALLS
+- Modify the wireframe (done)
+- get country data (done)
+- Show country data on the map (done)
+- Show country data on a table (done)
+
+
+COVID PART -4 COACHING CALL
+
+-
+-
+-
---
+++ script.js
@@ -0,0 +1,135 @@
+
+
+window.onload = () => {
+    getCountryData ();
+    buildChart();
+  
+}
+
+var map;
+var infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 39.8283, lng: -98.5795},
+  zoom: 3,
+  styles: mapStyle
+
+
+ });
+ infoWindow = new google.maps.InfoWindow();
+
+}
+
+const getCountryData = () => {
+    fetch ("https://corona.lmao.ninja/v2/countries")                      // fetch is way to get data from the API . fetch is a git
+    .then ((response) => {
+        return response.json()
+    }) .then((data)=>{
+        showDataOnMap(data);
+        showDataInTable(data);
+    })
+}
+
+const buildChart = () => {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+ });
+
+
+}
+
+const showDataOnMap = (data) => {
+   
+    data.map((country)=> {
+        let countryCenter = {
+          lat: country.countryInfo.lat,
+          lng: country.countryInfo.long
+        }
+
+
+        var countryCircle = new google.maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+            map: map,
+            center: countryCenter,
+            radius: country.casesPerOneMillion *15
+          });
+
+          var html = `
+          <div class="info-container">
+             <div class="info-flag" style="background-image: url(${country.countryInfo.flag});">
+                
+             </div>
+             <div class="info-name">
+             ${country.country}
+             </div>
+             <div class="info-confirmed">
+             Total: ${country.cases}
+             </div>
+             <div class="info-recovered">
+             Recovered: ${country.recovered}
+             </div>
+             <div class="info-deaths">
+             Deaths: ${country.deaths}
+             </div>
+
+          </div>
+          
+          `
+
+          var infoWindow = new google.maps.InfoWindow({
+              content: html,
+              position: countryCircle.center
+
+          });
+
+         
+          google.maps.event.addListener (countryCircle, 'mouseover', function() {
+              infoWindow.open(map);
+
+        });
+
+        google.maps.event.addListener(countryCircle, 'mouseout', function(){
+           infoWindow.close(); 
+        })
+
+
+
+    })
+
+}
+
+const showDataInTable = (data) => {
+    var html = '';
+    data.forEach((country)=> {
+        html += `
+        <tr>
+                <td>${country.country}</td>
+                <td>${country.cases}</td>
+                <td>${country.recovered}</td>
+                <td>${country.deaths}</td>
+            </tr>
+        `
+    })
+
+    document.getElementById('table-data').innerHTML = html;
+}
